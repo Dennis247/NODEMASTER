f82b273b465a52cfcfab7f5047ff3fc60ae6d99a
Dennis247 <denghazi619@gmail.com>
2019-10-27 18:10:49 +0100

    expressjs integration

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop');
+const path = require('path');
+const rootDir = require('./utils/path');
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoute);
 app.use(shopRoute);
 app.use((req, res, next) => {
-    res.status(404).send('<h4>Page Not Found</h4>')
+    res.status(404).sendFile(path.join(__dirname, 'Views', '404.html'));
 })
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const path = require("path");
+const rootDir = require('../utils/path');
 
 router.get("/add-product", (req, res, next) => {
-    res.send('<form action="/admin/product" method = "POST"><input type="text" name="title"/><button type="submit">Add Product<button/></from>');
+    res.sendFile(path.join(rootDir, 'Views', 'add-product.html'));
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
